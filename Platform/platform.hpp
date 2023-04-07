b4b3dfbe6c266a4ae8036a3a5b437b579b7b9e2e
iamadithedev <iusca.adrian@gmail.com>
2023-04-07 21:35:43 +0300

    Added Window class

--- Platform/platform.hpp
+++ Platform/platform.hpp
@@ -5,11 +5,10 @@ class Platform
 public:
     explicit Platform(int32_t major = 3, int32_t minor = 3, int32_t samples = 4);
 
-    bool init()    const;
+    [[nodiscard]] bool init() const;
 
     void update()  const;
     void release() const;
-
     void vsync()   const;
 
 private:
---
+++ Platform/window.cpp
@@ -0,0 +1,34 @@
+#include "window.hpp"
+
+Window::Window()
+    : _handle { }
+{
+}
+
+bool Window::create(const std::string& title, int32_t width, int32_t height)
+{
+    _handle  = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+               glfwMakeContextCurrent(_handle);
+
+    return _handle != nullptr;
+}
+
+void Window::update() const
+{
+    glfwSwapBuffers(_handle);
+}
+
+void Window::destroy() const
+{
+    glfwDestroyWindow(_handle);
+}
+
+bool Window::closing() const
+{
+    return glfwWindowShouldClose(_handle);
+}
+
+GLFWwindow* Window::handle() const
+{
+    return _handle;
+}
