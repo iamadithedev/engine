fed00350c1ef3511b1f8f12cfd93356f6041c460
iamadithedev <iusca.adrian@gmail.com>
2023-04-23 12:16:33 +0300

    add orthographic projection for camera class

--- Components/camera.cpp
+++ Components/camera.cpp
@@ -5,6 +5,11 @@ void Camera::perspective(float fov, float ratio, float near, float far)
     _projection = glm::perspective(glm::radians(fov), ratio, near, far);
 }
 
+void Camera::ortho(float left, float right, float bottom, float top)
+{
+    _projection = glm::ortho(left, right, bottom, top);
+}
+
 const glm::mat4& Camera::projection() const
 {
     return _projection;
--- Components/camera.hpp
+++ Components/camera.hpp
@@ -4,6 +4,7 @@ class Camera
 {
 public:
     void perspective(float fov, float ratio, float near = 0.1f, float far = 100.0f);
+    void ortho(float left, float right, float bottom, float top);
 
     [[nodiscard]] const glm::mat4& projection() const;
 
