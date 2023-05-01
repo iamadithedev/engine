c12e4688b3dfe66598fd01bc7cf58708674fc3bc
iamadithedev <iusca.adrian@gmail.com>
2023-05-01 23:44:41 +0300

    some work on the camera

---
+++ Core/Math/vec4.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+struct vec4
+{
+    float x;
+    float y;
+    float z;
+    float w;
+};
--- Scene/Components/camera.hpp
+++ Scene/Components/camera.hpp
@@ -3,11 +3,23 @@
 class Camera
 {
 public:
-    void perspective(float fov, float ratio, float near = 0.1f, float far = 100.0f);
-    void ortho(float left, float right, float bottom, float top);
+    Camera(float field_of_view, float aspect_ratio, float near_plane = 0.1f, float far_plane = 100.0f);
+
+    void resize(float width, float height);
+
+    void field_of_view(float value);
+    void near_plane(float value);
+    void far_plane(float value);
 
     [[nodiscard]] const glm::mat4& projection() const;
 
+    [[nodiscard]] float field_of_view() const;
+
 private:
     glm::mat4 _projection;
+
+    float _field_of_view;
+    float _aspect_ratio;
+    float _near_plane;
+    float _far_plane;
 };
