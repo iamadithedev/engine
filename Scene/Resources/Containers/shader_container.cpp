e5da984cbce71f0c44f24fae65dae25e37e82ebf
iamadithedev <iusca.adrian@gmail.com>
2023-05-21 15:52:11 +0300

    small change

--- Scene/Resources/Containers/shader_container.cpp
+++ Scene/Resources/Containers/shader_container.cpp
@@ -12,8 +12,10 @@ Shader* ShaderContainer::load(const std::string& file, const std::string& path)
 {
     const auto& root_node   = YAML::LoadFile(path + file);
     const auto& stages_node = root_node["stages"];
-          auto  type_value  = root_node["type"].as<std::size_t>();
-          assert(type_value == _hash);
+
+    auto binary_value = root_node["binary"].as<bool>();
+    auto type_value   = root_node["type"].as<std::size_t>();
+    assert(type_value == _hash);
 
     auto shader = new Shader(); // TODO here from a pool? is container a pool?
          shader->create();
